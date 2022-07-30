42466337ed53b09205b197b25052a01464db5501
aris402 <aris4455@hotmail.com>
2022-07-30 14:24:44 -0300

    Adicionando barra de navegação

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,85 @@
 import { useState, useEffect } from "react";
-import Box from '@mui/material/Box';
 import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
 import Divider from '@mui/material/Divider';
+import './css/styles.css';
 
 
 const App = (props: any) => {
-	const styles = {
+	const alignMent = {
 		display: 'flex',
 		alignItems: 'center',
 		flexDirection: 'column',
+	}
+	const navBar = {
+		bgcolor: 'rgb(18, 18, 18)',
+		color: 'white',
 	}
 	const [users, setUsers] = useState([
 		{ id: 1, name: "Minora" },
 		{ id: 2, name: "Ataide" },
 	]);
-  const [loading, setLoading] = useState(true);
 
-	useEffect(() => {
-		fetch("https://jsonplaceholder.typicode.com/users/")
-			.then((response) => response.json())
-			.then((json) => {setUsers(json); setLoading(false)});
+	const [loading, setLoading] = useState(true);
+
+		useEffect(() => {
+			fetch("https://jsonplaceholder.typicode.com/users/")
+				.then((response) => response.json())
+				.then((json) => {setUsers(json); setLoading(false)});
 	});
+	const [open, setOpen] = useState(false);
+	const isOpen = () => {
+		setOpen(true);
+	};
+	
+	  const isClosed = () => {
+		setOpen(false);
+	};
+
+
 	return (
-		<div className="App" style={styles}>
-			<h1>Lista de usuários</h1>
-			<div className="card">
-        {loading ? <h2>Carregando...</h2> : null}
-				<List>
-					{users.map((user) => (
+		<div className="App">
+				<Toolbar sx={navBar}>
+					<IconButton  edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+						<MenuIcon onClick={open ? isClosed : isOpen} />
+					</IconButton>
+					<Typography variant="h5" noWrap component="div" sx={{ flexGrow: 1 }}>
+						Navegação
+					</Typography>
+				</Toolbar>
+				{open ? 
+					<List className="nav-bar">
 						<ListItemButton>
-							<ListItemText primary={user.name}></ListItemText >
+							<ListItemText>Tarefas</ListItemText>
 						</ListItemButton>
-					))}
-				</List>
+						<ListItemButton>
+							<ListItemText>Posts</ListItemText>
+						</ListItemButton>
+					</List>
+					: null
+				}
+			<div className="List" style={alignMent}>
+				<h1>Lista de usuários</h1>
+				<div className="card">
+				{loading ? <h2>Carregando...</h2> : null}
+					<List>
+						{users.map((user) => (
+							<ListItemButton>
+								<ListItemText primary={user.name}></ListItemText >
+							</ListItemButton>
+						))}
+					</List>
+				</div>
 			</div>
 		</div>
 	);
